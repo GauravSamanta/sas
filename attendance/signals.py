75e7fc400dbba4b06d580427702a4311683e2c38
GauravSamanta <gauravsamanta143@gmail.com>
2023-10-07 17:03:35 +0530

    minor changes in signals.py in attendance

--- attendance/signals.py
+++ attendance/signals.py
@@ -8,7 +8,6 @@ from attendance.models import Assign_cls, AssignTime, AttendanceClass
 @receiver(post_save, sender = AssignTime)
 def creater_attendanceclass(sender, instance, created, **kwargs):
     if created:
-        ass = Assign_cls.objects.get()
         now1 = datetime.date.today()
 
         att, created = AttendanceClass.objects.update_or_create(assign = instance, date = now1)
